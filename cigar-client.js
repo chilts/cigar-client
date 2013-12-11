f21bb6689d609aed106d60527dbcdf0be5d164c9
Andrew Chilton <andychilton@gmail.com>
2013-12-11 21:57:14 +1300

    Server now working (somewhat)

--- cigar-client.js
+++ cigar-client.js
@@ -13,132 +13,26 @@
 // ----------------------------------------------------------------------------
 
 // core
+var fs = require('fs');
 var http = require('http');
-var url = require('url');
 
 // npm
-var async = require('async');
-var nconf = require('nconf');
-var level = require('level');
-var flake = require('flake')('eth0');
+var ini   = require('ini');
+var paramify = require('paramify');
 
 // local
-var cfg = require('./lib/cfg.js')();
-
-// ----------------------------------------------------------------------------
-// plugins
-
-var plugin = {};
-
-// loop through which plugins we want loaded
-var plugins = process.env.CIGAR_PLUGINS || ['os', 'mem'];
-plugins.forEach(function(pluginName) {
-    plugin[pluginName] = require('cigar-plugin-' + pluginName);
-});
-
-// ----------------------------------------------------------------------------
-// the datastore
-
-// open the LevelDB
-var db = level(cfg.store, { valueEncoding : 'json' });
-
-// ----------------------------------------------------------------------------
-// stats collection
-
-// collect stats every 60 seconds
-var boundary = 60 * 1000;
-
-// returns the number of milliseconds until the next stat collection
-function getNextStatTrigger() {
-    return boundary - (Date.now() % boundary);
-}
-
-// gether() - will get everything together and put it into LevelDB
-function gather() {
-    // get the boundary this particular gathering should be for
-    var now  = Date.now();
-    var date = new Date(now - (now % boundary));
-
-    plugins.forEach(function(pluginName) {
-        // gether these stats
-        plugin[pluginName](function(err, info) {
-            if (err) {
-                console.error(err);
-                return;
-            }
-
-            log(pluginName, info);
-
-            var key = pluginName + '!' + date.toISOString();
-            db.put(key, info, function(err) {
-                if (err) return console.error(err);
-            });
-        });
-    });
-
-    // let's do this again in a minute
-    setTimeout(gather, getNextStatTrigger() );
-}
-
-// trigger the first timeout on the next minute boundary
-setTimeout(gather, getNextStatTrigger() );
+var cfg = require('./lib/cfg.js');
+var app = require('./lib/app.js');
 
 // ----------------------------------------------------------------------------
 // server
 
 var server = http.createServer();
-server.on('request', function(req, res) {
-    var data;
+server.on('request', app);
 
-    // figure out which plugin to do
-    var pluginName = url.parse(req.url).pathname.substr(1);
-
-    if ( pluginName === '' ) {
-        // just list all the plugins available on this client
-        data = {
-            ok   : true,
-            info : Object.keys(plugin),
-        };
-        log('home', Object.keys(plugin));
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(data) + "\n");
-    }
-    else if ( pluginName === 'all' ) {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify({ ok : false, err : 'ToDo' }) + "\n");
-        log('all', 'ToDo');
-    }
-    else if ( plugin[pluginName] ) {
-        plugin[pluginName](function(err, info) {
-            data = {
-                ok   : true,
-                info : info,
-            };
-            log(pluginName, info);
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.end(JSON.stringify(data) + "\n");
-        });
-    }
-    else {
-        data = {
-            ok  : false,
-            err : 'Unknown plugin ' + pluginName,
-        };
-        log(pluginName, 'Unknown plugin');
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(data) + "\n");
-    }
-});
-
-var port = process.env.CIGAR_CLIENT_PORT || 9599;
+var port = process.env.CIGAR_CLIENT_PORT || cfg.port || 9123;
 server.listen(port, function() {
     console.log('Listening on port %s', port);
 });
-
-// ----------------------------------------------------------------------------
-
-function log(pluginName, info) {
-    console.log((new Date()).toISOString() + ' - ' + pluginName + ' : ' + JSON.stringify(info));
-}
 
 // ----------------------------------------------------------------------------
